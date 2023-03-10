f12ba5e3968691ca1a4a28887ab15c1c4db2da6d
junyoung011019 <kidjjy5@naver.com>
2023-03-10 23:49:09 +0900

    230310
    자바스크립트 일부 추가

---
+++ search.js
@@ -0,0 +1,18 @@
+var rbutton=document.getElementById("b1")
+
+rbutton.addEventListener("click",function(){
+    rbutton.classList.add("clicked") 
+})
+
+var tbutton=document.getElementById("b2")
+
+tbutton.addEventListener("click",function(){
+    tbutton.classList.add("clicked") 
+})
+
+var cbutton=document.getElementById("b3")
+
+cbutton.addEventListener("click",function(){
+    cbutton.classList.add("clicked") 
+})
+
