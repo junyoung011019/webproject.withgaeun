d359e75613aac8375561891a1777e6407b3f85a9
junyoung011019 <kidjjy5@naver.com>
2023-03-11 23:40:58 +0900

    230311
    검색페이지 80프로 완성!

--- search.js
+++ search.js
@@ -1,18 +1,34 @@
-var rbutton=document.getElementById("b1")
+var b1=document.getElementById("b1")
+var b2=document.getElementById("b2")
+var b3=document.getElementById("b3")
 
-rbutton.addEventListener("click",function(){
-    rbutton.classList.add("clicked") 
-})
-
-var tbutton=document.getElementById("b2")
-
-tbutton.addEventListener("click",function(){
-    tbutton.classList.add("clicked") 
-})
-
-var cbutton=document.getElementById("b3")
-
-cbutton.addEventListener("click",function(){
-    cbutton.classList.add("clicked") 
-})
-
+b1.onclick=function(){
+    document.getElementById("b1i").src="images/식당 1.png";
+    document.getElementById("b2i").src="images/관광지 0.png";
+    document.getElementById("b3i").src="images/카페 0.png";
+    document.getElementById("T1").style.color="black";
+    document.getElementById("T2").style.color="#D3D3D3";
+    document.getElementById("T3").style.color="#D3D3D3";
+    document.getElementById("mb").disabled=false;
+    document.getElementById("nb").disabled=false;
+}
+b2.onclick=function(){
+    document.getElementById("b1i").src="images/식당 0.png";
+    document.getElementById("b2i").src="images/관광지 1.png";
+    document.getElementById("b3i").src="images/카페 0.png";
+    document.getElementById("T1").style.color="#D3D3D3";
+    document.getElementById("T2").style.color="black";
+    document.getElementById("T3").style.color="#D3D3D3";
+    document.getElementById("mb").disabled=false;
+    document.getElementById("nb").disabled=false;
+}
+b3.onclick=function(){
+    document.getElementById("b1i").src="images/식당 0.png";
+    document.getElementById("b2i").src="images/관광지 0.png";
+    document.getElementById("b3i").src="images/카페 1.png";
+    document.getElementById("T1").style.color="#D3D3D3";
+    document.getElementById("T2").style.color="#D3D3D3";
+    document.getElementById("T3").style.color="black";
+    document.getElementById("mb").disabled=false;
+    document.getElementById("nb").disabled=false;
+}
