be9c0b583d7d51f4711b1ea620283fa50d1b757a
junyoung011019 <kidjjy5@naver.com>
2023-03-23 16:50:49 +0900

    230323
    write까지 일단 수정완료

--- write.js
+++ write.js
@@ -3,7 +3,7 @@ var wy_button=document.getElementById("wy_button")
 wy_button.onclick=function(){
     if(confirm("등록 하시겠습니까?")){
         alert("등록 되었습니다.")
-        location.href="marketmenu.html"
+        history.back()
     }else{
         return;
     }
@@ -13,7 +13,7 @@ var wc_button=document.getElementById("wc_button")
 
 wc_button.onclick=function(){
     if(confirm("등록을 취소 하시겠습니까?")){
-        location.href="marketmenu.html"
+        history.back()
     }else{
         return;
     }
